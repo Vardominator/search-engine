455179b754dbb840aacf0c36511dbd917cb9ced7
nickjmiller <nickamilli@gmail.com>
2017-09-17 22:26:09 -0700

    Added ngram lookup function

--- kgram.py
+++ kgram.py
@@ -1,11 +1,13 @@
+"""KGramIndex class file"""
+
 class KGramIndex(object):
     """Pydoc inc, going to work on this a lot more but base functionality is here"""
     def __init__(self, num_grams):
         self.num_grams = num_grams
         self.index = {}
 
-    """Maps word to kgrams"""
     def map_ngram(self, word):
+        """Maps word to kgrams"""
         grams = zip(*[word[i:] for i in range(self.num_grams)])
         for gram in grams:
             gram_str = ''.join(gram)
@@ -14,7 +16,26 @@ class KGramIndex(object):
             elif word not in self.index[gram_str]:
                 self.index[gram_str].append(word)
 
-    """Iterates through input list to construct kgrams"""
-    def index_list(self, input_list):
-        for word in input_list:
-            self.map_ngram(word)
+    def add_to_index(self, input_list):
+        """Iterates through input list to construct kgram index"""
+        if input_list:
+            for word in input_list:
+                self.map_ngram(word)
+        else:
+            print('Empty list passed to kgram parser.')
+
+    def get_words(self, gram):
+        """Returns a list of all words found containing kgram from index"""
+        if gram in self.index:
+            return self.index[gram]
+        else:
+            print("Key not found in kgram index.")
+            return None
+
+    @staticmethod
+    def print_kgrams(kgram_number, word):
+        """Helper function to list kgrams for individual words"""
+        grams = set(zip(*[word[i:] for i in range(kgram_number)]))
+        print('{}-Grams found in {}:'.format(kgram_number, word))
+        for gram in grams:
+            print(''.join(gram))
--- test/test_kgram.py
+++ test/test_kgram.py
@@ -1,14 +1,44 @@
-import pytest
+"""Unit test file for kgram.py"""
 import kgram
 
+TEST_LIST = ['test', 'test2', 'other']
+
 def test_initialize_kgram():
+    """Tests creation of class, asserts kgram # is assigned"""
     kgram_test = kgram.KGramIndex(2)
     assert kgram_test.num_grams == 2
 
 
 def test_map_ngram_keys_created():
-    word = "test"
+    """Tests kgram mapping with one word"""
+    word = TEST_LIST[0]
     kgram_test = kgram.KGramIndex(3)
-    answer = {"tes":["test"], "est": ["test"]}
     kgram_test.map_ngram(word)
+    answer = {'tes':['test'], 'est': ['test']}
     assert answer == kgram_test.index
+
+def test_add_to_index():
+    """Tests mapping over whole input list"""
+    kgram_test = kgram.KGramIndex(3)
+    kgram_test.add_to_index(TEST_LIST)
+    answer = {'tes':['test', 'test2'], 'est':['test', 'test2'], 'st2':['test2'],
+              'oth':['other'], 'the':['other'], 'her':['other']}
+    assert kgram_test.index == answer
+
+def test_add_to_index_null_list():
+    """Tests that call to add_to_index handles empty list"""
+    empty_list = []
+    kgram_test = kgram.KGramIndex(3)
+    kgram_test.add_to_index(empty_list)
+
+def test_get_words_gets_words():
+    """Tests that word retrieval works for initialized index"""
+    kgram_test = kgram.KGramIndex(3)
+    kgram_test.add_to_index(TEST_LIST)
+    answer = {'test', 'test2'}
+    assert set(kgram_test.get_words('est')) == answer
+
+def test_get_words_handles_no_key():
+    """Tests that no issues raised when calling missing key"""
+    kgram_test = kgram.KGramIndex(3)
+    kgram_test.get_words('not in index')
