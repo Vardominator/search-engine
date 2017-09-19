7384a09c13bf1f5d690901f4ba06bcd2aa42be82
nickjmiller <nickamilli@gmail.com>
2017-09-18 20:51:44 -0700

    Fixed logic in dehyphenation

--- normalize.py
+++ normalize.py
@@ -1,7 +1,7 @@
 """Normalization class"""
-from porter2stemmer import Porter2Stemmer
-from string import punctuation
 import re
+
+from porter2stemmer import Porter2Stemmer
 
 class Normalizer(object):
     def __init__(self):
@@ -9,23 +9,22 @@ class Normalizer(object):
 
     def normalize(self, word):
         word = self.remove_special_characters(word)
-        word_list = self.dehyphenate(word)
-        for token in word_list:
-            token = self.stem(token)
-        return word_list
+        word_set = self.dehyphenate(word)
+        word_set = [self.stem(token) for token in word_set]
+        return word_set
 
     def stem(self, word):
         return self.stemmer.stem(word.lower())
 
     def remove_special_characters(self, word):
         word = re.sub(r'^\W+|\W+$', '', word)
-        word = word.replace("'","")
+        word = word.replace("'", "")
         return word
 
     def dehyphenate(self, word):
         if '-' not in word:
-            return [word]
+            return {word}
         else:
             word_list = word.split('-')
             word_list.append(word)
-            return set(word_list.append(word))
+            return set(word_list)
