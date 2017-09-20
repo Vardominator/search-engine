436f1bf07fb145dc680362b6f8bef07fe07bbea8
nickjmiller <nickamilli@gmail.com>
2017-09-19 17:34:57 -0700

    Added kgram intersection, grams from 1 to k

--- kgram.py
+++ kgram.py
@@ -2,19 +2,23 @@
 
 class KGramIndex(object):
     """Pydoc inc, going to work on this a lot more but base functionality is here"""
-    def __init__(self, num_grams):
+    def __init__(self, num_grams, vocab = None):
         self.num_grams = num_grams
         self.index = {}
+        if vocab:
+            self.add_to_index(vocab)
 
     def map_ngram(self, word):
-        """Maps word to kgrams"""
-        grams = zip(*[word[i:] for i in range(self.num_grams)])
-        for gram in grams:
-            gram_str = ''.join(gram)
-            if gram_str not in self.index:
-                self.index[gram_str] = [word]
-            elif word not in self.index[gram_str]:
-                self.index[gram_str].append(word)
+        """Maps word to kgrams from 1 to k"""
+        gram_word = "$" + word + "$"
+        grams = [zip(*[gram_word[i:] for i in range(num_grams)]) for num_grams in range(1, self.num_grams+1)]
+        for gram_set in grams:
+            for gram in gram_set:
+                gram_str = ''.join(gram)
+                if gram_str not in self.index:
+                    self.index[gram_str] = [word]
+                elif word not in self.index[gram_str]:
+                    self.index[gram_str].append(word)
 
     def add_to_index(self, input_list):
         """Iterates through input list to construct kgram index"""
@@ -29,13 +33,21 @@ class KGramIndex(object):
         if gram in self.index:
             return self.index[gram]
         else:
-            print("Key not found in kgram index.")
-            return None
+            print("Key not found in kgram index: {}".format(gram))
+            return []
+
+    def get_intersection_grams(self, grams):
+        gram_sets = []
+        for gram in grams:
+            gram_sets.append(set(self.get_words(gram)))
+        return set.intersection(*gram_sets)
+
 
     @staticmethod
     def print_kgrams(kgram_number, word):
         """Helper function to list kgrams for individual words"""
-        grams = set(zip(*[word[i:] for i in range(kgram_number)]))
+        gram_word = "$" + word + "$"
+        grams = set(zip(*[gram_word[i:] for i in range(kgram_number)]))
         print('{}-Grams found in {}:'.format(kgram_number, word))
         for gram in grams:
             print(''.join(gram))
