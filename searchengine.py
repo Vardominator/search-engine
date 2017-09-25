8a05298ae83ae6a8453c6b444a10b68f41d77dbe
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-09-24 20:56:48 -0700

    Made massive progress with GUI. directory can now be set to build the index.  This returns the document count as well as the term count.  Queries return the relevant documents. For now the relevant document ids and the respective filenames are returned as a json and console printed in javascript.  Tomorrow I will work on listing the returned document file names in a table or something.

--- searchengine.py
+++ searchengine.py
@@ -8,24 +8,63 @@ import queryprocessing
 
 app = Flask(__name__)
 
+index = None
+doc_id_files = {}
+
 @app.route('/test', methods=['GET', 'POST'])
 def home():
     return json.dumps({'response': "hello"})
 
 @app.route('/buildindex', methods=['GET', 'POST'])
 def buildindex():
-    corpus_dir = 'data/documents'
-    test_docs_dir = 'data/testdocuments'
-    sample_docs = []
+    if request.method == 'POST':
+        docs = []
+        docs_dir = request.form['corpus_dir']
 
-    for root,dirs,files in os.walk(test_docs_dir):
-        files = sorted(files)
-        for file in files:
-            with open(os.path.join(test_docs_dir, file), 'r') as f:
-                sample_docs.append(f.read())
+        id = 0
+        for root,dirs,files in os.walk(docs_dir):
+            files = sorted(files)
+            for file in files:
+                doc_id_files[id] = file
+                id += 1
+                with open(os.path.join(docs_dir, file), 'r') as f:
+                    docs.append(f.read())
 
-    index = indexing.create_index(sample_docs)
-    return json.dumps({'terms': list(index.keys())})
+        index = indexing.create_index(docs)
+
+        # STORE IN CONTEXT
+        app.config['index'] = index
+        app.config['doc_id_files'] = doc_id_files
+
+        return json.dumps({
+                            'files': files,
+                            'doc_count': len(files),
+                            'terms': list(index.keys()),
+                            'term_count': len(index)
+                          })
+
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    if request.method == 'POST':
+
+        index = app.config['index']
+        doc_id_files = app.config['doc_id_files']
+
+        query = request.form['query']
+        
+        literals = queryprocessing.process_query(query)
+        search_results = queryprocessing.query_search(literals, index)
+        
+        files = []
+        
+        for doc_id in search_results:
+            files.append(doc_id_files[doc_id])
+        print(files)
+        return json.dumps({
+                            'doc_ids': search_results,
+                            'files': files,
+                          })
 
 if __name__ == "__main__":
     app.run(debug = True)
--- test.js
+++ test.js
@@ -1,23 +1,52 @@
 $(document).ready(function(){
+
+    $('#dir_input').change(function(e){
+        if($('#dir_input').val() != ''){
+            $('#buildindex_button').removeClass('disabled');
+        }else{
+            $('#buildindex_button').addClass('disabled');
+        }
+    });
+    
     $("#buildindex_button").click(function(e){
+        var dir = $('#dir_input').val();
         $.ajax({
             type: "POST",
             url: "http://127.0.0.1:5000/buildindex",
-            success: successCallBack
+            data : {corpus_dir: dir},
+            success: populateTable
         });
+        $('#dir_input').val("");
+        $('#buildindex_button').addClass('disabled');
     });
 
-    $("#search_button").click(function(e){
-        console.log("button clicked asdasdf");
+    $("#query").change(function(e){
+        var query_input = $('#query').val();
+
+        //if(query_input != ''){
+        var query_input = $('#query').val();
+        console.log(query_input);
         $.ajax({
             type: "POST",
-            url: "http://127.0.0.1:5000/test",
+            url: "http://127.0.0.1:5000/query",
+            data : {query: query_input},
             success: successCallBack
         });
+        //}
+
+        $('#query').val("");
     });
+
+    function populateTable(response){
+        console.log(response.doc_count);
+        var res = $.parseJSON(response);
+        $("#document_count").text(res.doc_count);
+        $("#term_count").text(res.term_count);
+    }
 
     function successCallBack(response){
         console.log(response);
     }
+
 
 });
