5148787ba30fd563bdd1bb93445d12e8ae103a0d
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-09-25 14:37:50 -0700

    GUI now shows documents that contain query literals and a document preview as shown in the image above

--- searchengine.py
+++ searchengine.py
@@ -19,22 +19,27 @@ def home():
 def buildindex():
     if request.method == 'POST':
         docs = []
+        file_contents = {}
         docs_dir = request.form['corpus_dir']
 
         id = 0
         for root,dirs,files in os.walk(docs_dir):
             files = sorted(files)
+            
             for file in files:
                 doc_id_files[id] = file
                 id += 1
                 with open(os.path.join(docs_dir, file), 'r') as f:
-                    docs.append(f.read())
+                    content = f.read()
+                    docs.append(content)
+                    file_contents[file] = content
 
         index = indexing.create_index(docs)
 
         # STORE IN CONTEXT
         app.config['index'] = index
         app.config['doc_id_files'] = doc_id_files
+        app.config['file_contents'] = file_contents
 
         return json.dumps({
                             'files': files,
@@ -50,20 +55,25 @@ def query():
 
         index = app.config['index']
         doc_id_files = app.config['doc_id_files']
+        file_contents = app.config['file_contents']
 
         query = request.form['query']
         
         literals = queryprocessing.process_query(query)
         search_results = queryprocessing.query_search(literals, index)
         
-        files = []
+        relevant_files = []
+        relevant_contents = {}
         
         for doc_id in search_results:
-            files.append(doc_id_files[doc_id])
-        print(files)
+            file = doc_id_files[doc_id]
+            relevant_files.append(file)
+            relevant_contents[file] = file_contents[file]
+
         return json.dumps({
                             'doc_ids': search_results,
-                            'files': files,
+                            'files': relevant_files,
+                            'contents': relevant_contents
                           })
 
 if __name__ == "__main__":
--- test.js
+++ test.js
@@ -23,25 +23,48 @@ $(document).ready(function(){
     $("#query").change(function(e){
         var query_input = $('#query').val();
 
-        //if(query_input != ''){
-        var query_input = $('#query').val();
-        console.log(query_input);
         $.ajax({
             type: "POST",
             url: "http://127.0.0.1:5000/query",
             data : {query: query_input},
-            success: successCallBack
+            success: buildRelevantList
         });
-        //}
 
         $('#query').val("");
+
     });
 
     function populateTable(response){
-        console.log(response.doc_count);
         var res = $.parseJSON(response);
+        console.log(res);
         $("#document_count").text(res.doc_count);
         $("#term_count").text(res.term_count);
+    }
+
+    function buildRelevantList(response){
+        var res = $.parseJSON(response);
+
+        $("#relevant_list").empty()
+        $.each(res.files, function(index, file){
+
+            content = res.contents[file];
+            if(content.length > 80){
+                content = content.substring(0, 100);
+                content = content + "...";
+            }
+
+            $("#relevant_list").append(
+            '<div class="item">' + 
+                '<i class="map marker icon"></i>' + 
+                '<div class="content">' +
+                    '<a class="header">' + file + '</a>' +
+                    '<div class="description">' +
+                        "blah" + content + 
+                    '</div>'
+                + '</div>'
+            + '</div>');
+        });1
+        
     }
 
     function successCallBack(response){
