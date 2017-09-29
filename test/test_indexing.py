1af4af798e89eab48dbc6b27cd19bb110da46e49
nickjmiller <nickamilli@gmail.com>
2017-09-29 16:16:27 -0700

    Unit tests completed for query processing and indexing

--- test/test_indexing.py
+++ test/test_indexing.py
@@ -21,35 +21,43 @@ def process_documents():
 docs = process_documents()
 
 TRUE_INDEX = {
-                "test":[indexing.PositionalPosting(1,[3]),
-                        indexing.PositionalPosting(2,[1]),
-                        indexing.PositionalPosting(4,[0, 1, 2, 3, 4]),
-                        indexing.PositionalPosting(5,[1])],
-                "document":[indexing.PositionalPosting(1,[4]),
-                            indexing.PositionalPosting(2,[2]),
-                            indexing.PositionalPosting(5,[0])],
-                "here":[indexing.PositionalPosting(2,[4]),
-                        indexing.PositionalPosting(3,[0]),
-                        indexing.PositionalPosting(5,[3])],
-                "we":[indexing.PositionalPosting(3,[1])],
-                "go":[indexing.PositionalPosting(3,[2])],
-                "goe":[indexing.PositionalPosting(5,[2])],
-                "anoth":[indexing.PositionalPosting(2,[0])],
-                "third":[indexing.PositionalPosting(3,[4])],
-                "this":[indexing.PositionalPosting(1,[0])],
-                "is":[indexing.PositionalPosting(1,[1]),
+              "test":[indexing.PositionalPosting(0,[3]),
+                      indexing.PositionalPosting(1,[1]),
+                      indexing.PositionalPosting(3,[0, 1, 2, 3, 4]),
+                      indexing.PositionalPosting(4,[1])],
+              "document":[indexing.PositionalPosting(0,[4]),
+                          indexing.PositionalPosting(1,[2]),
+                          indexing.PositionalPosting(4,[0])],
+              "here":[indexing.PositionalPosting(1,[4]),
+                      indexing.PositionalPosting(2,[0]),
+                      indexing.PositionalPosting(4,[3])],
+              "we":[indexing.PositionalPosting(2,[1])],
+              "go":[indexing.PositionalPosting(2,[2])],
+              "goe":[indexing.PositionalPosting(4,[2])],
+              "anoth":[indexing.PositionalPosting(1,[0])],
+              "third":[indexing.PositionalPosting(2,[4])],
+              "this":[indexing.PositionalPosting(0,[0])],
+              "is":[indexing.PositionalPosting(0,[1]),
+                      indexing.PositionalPosting(1,[3])],
+              "a":[indexing.PositionalPosting(0,[2]),
                       indexing.PositionalPosting(2,[3])],
-                "a":[indexing.PositionalPosting(1,[2]),
-                     indexing.PositionalPosting(3,[3])],
-                "one":[indexing.PositionalPosting(3,[5])]
+              "one":[indexing.PositionalPosting(2,[5])]
              }
 TRUE_INDEX = collections.OrderedDict(sorted(TRUE_INDEX.items(), key=lambda t:t[0]))
 
 index = indexing.create_index(docs)[0]
 
 def test_index_has_all_keys():
+    """Checks to make sure the index has the right number of keys"""
     assert(set(index.keys()) == set(TRUE_INDEX.keys()))
 
 def test_index_has_all_postings():
+    """Checks each word in the index to see if it has the right 
+       number of document postings"""
     for word in index.keys():
         assert len(index[word]) == len(TRUE_INDEX[word])
+
+def test_index_collects_all_positions_in_document():
+    """Checks to make sure the index captures all positions for a
+       term appearing multiple times"""
+    assert index['test'][2].postings_list == (3, [0, 1, 2, 3, 4])
--- test/test_queryprocessing.py
+++ test/test_queryprocessing.py
@@ -1,7 +1,49 @@
-"""Unit Tests for special queries, should move over to queryprocessing later"""
+"""Unit Tests for queries"""
+import os
+import json
 import queryprocessing
+import indexing
 
-# Bad practice importing kgram, need to mock call or something
+def process_documents():
+    """Helper function to pre-process documents"""
+    docs = []
+    docs_dir = 'test/test_docs'
+
+    for root,dirs,files in os.walk(docs_dir):
+        files = sorted(files)
+
+        for file in files:
+            with open(os.path.join(docs_dir, file), 'r') as json_data:
+                content = json.load(json_data)
+                docs.append(content['body'])
+    return docs
+
+docs = process_documents() 
+
+pos_index = indexing.create_index(docs)[0]
+
+def test_standard_query():
+    query = 'test'
+    ans = [0, 1, 3, 4]
+    literals = queryprocessing.process_query(query, None)
+    assert ans == queryprocessing.query_search(literals, pos_index)
+
+def test_phrase_query():
+    phrase_query = '\"third one\"'
+    literals = queryprocessing.process_query(phrase_query, None)
+    queryprocessing.query_search(literals, pos_index)
+
+def test_and_queries():
+    and_query = 'test + document'
+    literals = queryprocessing.process_query(and_query, None)
+    queryprocessing.query_search(literals, pos_index)
+
+def test_complex_query():
+    phrase_and_query = '\"test document\"+this'
+    literals = queryprocessing.process_query(phrase_and_query, None)
+    queryprocessing.query_search(literals, pos_index)
+
+# Testing wildcard queries
 from kgram import KGramIndex
 k_index = KGramIndex(3)
 k_index.add_to_index(['test', 'best', 'tempest'])
