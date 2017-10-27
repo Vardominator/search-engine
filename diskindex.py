3782dc4bc9ed6e46bdcb91852c63918acc286ee6
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-10-26 18:10:24 -0700

    disk index now uses B+ tree (sqlite), will remove comments and stuff when I get back from jiu-jitsu tonight

--- diskindex.py
+++ diskindex.py
@@ -1,5 +1,7 @@
 import pickle
 import shlex
+import sqlite3
+
 from itertools import chain, groupby
 from operator import itemgetter
 import collections
@@ -21,58 +23,55 @@ class IndexWriter(object):
         """Calls member methods to write vocab and postings to disk."""
         dictionary = list(index.keys())
         vocab_positions = [None]*len(dictionary)
-        self.write_vocab(self.path, dictionary, vocab_positions)
+        # self.write_vocab(self.path, dictionary, vocab_positions)
         self.write_postings(self.path, index, dictionary, vocab_positions)
 
     def write_vocab(self, path, dictionary, vocab_positions):
         """Writes vocab to disk and stores positions in a list."""
-        vocab_file = open('{}vocab.bin'.format(path), 'wb')
+        # vocab_file = open('{}vocab.bin'.format(path), 'wb')
+        # vocab_index = 0
+        # vocab_position = 0
+        # for term in dictionary:
+        #     vocab_positions[vocab_index] = vocab_position
+        #     vocab_file.write(term.encode())
+        #     vocab_index += 1
+        #     vocab_position += len(term.encode())
+        # vocab_file.close()
+        conn = sqlite3.connect('bin/vocabtable.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE vocab (term TEXT, position INTEGER)''')
         vocab_index = 0
         vocab_position = 0
         for term in dictionary:
             vocab_positions[vocab_index] = vocab_position
-            vocab_file.write(term.encode())
+            c.execute("INSERT INTO vocab VALUES ('{}', {})".format(term, vocab_position))
+            conn.commit()
             vocab_index += 1
             vocab_position += len(term.encode())
-        vocab_file.close()
-
-        # # test with reading
-        # vocab_file = open('bin/vocab.bin', 'rb')
-        # last_position = vocab_positions[0]
-        # vocab_words = []
-        # for position in vocab_positions[1:]:
-        #     vocab_bytes = vocab_file.read(position - last_position)
-        #     vocab_words.append(vocab_bytes)
-        #     vocab_file.seek(position, 0)
-        #     last_position = position
-
-        # print(vocab_words[0:1000])
-
-        # vocab_file = open('bin/vocabtable.bin', 'rb')
-        # vocab_position = 0
-        # # read first four bytes
-        # vocab_size_bytes = vocab_file.read(4)
-        # vocab_size_int = int.from_bytes(vocab_size_bytes, byteorder='big')
-        # print(vocab_size_int)
-        # vocab_file.close()
-
+        conn.close()
 
     def write_postings(self, path, index, dictionary, vocab_positions):
         """Writes postings to disk."""
-        vocab_table_file = open('{}vocabtable.bin'.format(path), 'wb')
+        # vocab_table_file = open('{}vocabtable.bin'.format(path), 'wb')
         postings_file = open('{}postings.bin'.format(path), 'wb')
 
-        # write number of terms in vocab (4 bytes)
-        vocab_table_file.write((len(dictionary)).to_bytes(4, byteorder='big'))
+        conn = sqlite3.connect('bin/vocabtable.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE vocabtable (term TEXT, position INTEGER)''')
 
-        vocab_index = 0
+        # write number of terms in vocab (4 bytes)
+        # vocab_table_file.write((len(dictionary)).to_bytes(4, byteorder='big'))
+
+        # vocab_index = 0
         for term in dictionary:
             postings = index[term]
-            # vocab position
-            vocab_table_file.write((vocab_positions[vocab_index]).to_bytes(8, byteorder='big'))
-            # position in postings file (8 bytes)
-            vocab_table_file.write((postings_file.tell()).to_bytes(8, byteorder='big'))
-            # number of documents (dft - 4 bytes)
+            # # vocab position
+            # vocab_table_file.write((vocab_positions[vocab_index]).to_bytes(8, byteorder='big'))
+            # # position in postings file (8 bytes)
+            # vocab_table_file.write((postings_file.tell()).to_bytes(8, byteorder='big'))
+            # # number of documents (dft - 4 bytes)
+            c.execute("INSERT INTO vocabtable VALUES (?, ?)", (term, postings_file.tell()))
+            conn.commit()
             postings_file.write((len(postings)).to_bytes(4, byteorder='big'))
 
             last_docid = 0
@@ -87,9 +86,10 @@ class IndexWriter(object):
                 for position in postings_list[1]:
                     postings_file.write((position).to_bytes(4, byteorder='big'))
 
-            vocab_index += 1
+            # vocab_index += 1
 
-        vocab_table_file.close()
+        conn.close()
+        # vocab_table_file.close()
         postings_file.close()
 
 
@@ -97,23 +97,17 @@ class DiskIndex(object):
     def __init__(self, path='bin/'):
         self.path = path
 
-    # QUERYING
-    # RETURN THREE VALUES PER DOCUMENT IN A TERM'S POSTING LIST: 
-    #     1. The document ID
-    #     2. The term frequency
-    #     3. Positions (optional)
-
-    # ASK NEAL IF YOU CAN USE THE POSITIONS FLAG INSTEAD OF HAVING A SEPARATE METHOD
     def retrieve_postings(self, query):
         """Retrieve postings lists with/without positional information"""
         query_literals = process_query(query)
-        vocab_table_file = open('{}vocabtable.bin'.format(self.path), 'rb')
-        vocab_file = open('{}vocab.bin'.format(self.path), 'rb')
+        # vocab_table_file = open('{}vocabtable.bin'.format(self.path), 'rb')
+        # vocab_file = open('{}vocab.bin'.format(self.path), 'rb')
         postings_file = open('{}postings.bin'.format(self.path), 'rb')
 
-        # TEMPORARY INDEX
-        index = {}
+        conn = sqlite3.connect('bin/vocabtable.db')
+        c = conn.cursor()
 
+        index = {}
         for literal in query_literals:
             all_terms = literal.replace('"', '').split()
             all_terms = [normalize.query_normalize(term) for term in all_terms]
@@ -124,67 +118,102 @@ class DiskIndex(object):
                 positions = True
 
             for subliteral in all_terms:
-                # add term to index
                 if subliteral not in index:
                     index[subliteral] = []
 
-                    # term count stored in first 4 bytes of file
-                    term_count_bytes = vocab_table_file.read(4)
-                    term_count = int.from_bytes(term_count_bytes, byteorder='big')
-
-                    # use linear search for now, will user B+ tree later
-                    last_position_bytes = vocab_table_file.read(8)
-                    last_position = int.from_bytes(last_position_bytes, byteorder='big')
-
-                    for i in range(term_count - 1):
-                        # READ POSTING POSITION
-                        posting_position_bytes = vocab_table_file.read(8)
-                        posting_position = int.from_bytes(posting_position_bytes, byteorder='big')
-                        # READ VOCAB POSITION
-                        vocab_position_bytes = vocab_table_file.read(8)
-                        vocab_position = int.from_bytes(vocab_position_bytes, byteorder='big')
-                        # READ WORD AT CURRENT VOCAB POSITION
-                        term = vocab_file.read(vocab_position-last_position)
-                        last_position = vocab_position
-                        # READ NUMBER OF DOCS AT CURRENT POSTING POSITION
+                    # QUERY SQL DB
+                    c.execute("SELECT * FROM vocabtable WHERE term=?", (subliteral,))
+                    row = c.fetchall()[0]
+                    # print(row)
+                    if len(row) > 0:
+                        posting_position = row[1]
                         postings_file.seek(posting_position, 0)
                         number_docs_bytes = postings_file.read(4)
                         number_docs = int.from_bytes(number_docs_bytes, byteorder='big')
-                        # FOR EACH DOC READ POSTINGS LIST
+                        # print(number_docs)
                         last_doc_id = 0
+                        for d in range(number_docs):
+                            doc_id_gap_bytes = postings_file.read(4)
+                            doc_id_gap = int.from_bytes(doc_id_gap_bytes, byteorder='big')
+                            # GET DOCID BY ADDING GAP
+                            last_doc_id += doc_id_gap
+                            # READ TERM FREQUENCY
+                            term_freq_bytes = postings_file.read(4)
+                            term_freq = int.from_bytes(term_freq_bytes, byteorder='big')
+                
+                            current_posting = [last_doc_id, term_freq]
+                            
+                            # READ TERM POSITIONS
+                            if positions:
+                                doc_positions = []
+                                # if positions desired read them
+                                for f in range(term_freq):
+                                    position_bytes = postings_file.read(4)
+                                    position = int.from_bytes(position_bytes, byteorder='big')
+                                    doc_positions.append(position)
+                                current_posting.append(doc_positions)
 
-                        # only doc id to index if current vocab term
-                        if term.decode() == subliteral:
-                            for d in range(number_docs):
-                                doc_id_gap_bytes = postings_file.read(4)
-                                doc_id_gap = int.from_bytes(doc_id_gap_bytes, byteorder='big')
-                                # GET DOCID BY ADDING GAP
-                                last_doc_id += doc_id_gap
-                                # READ TERM FREQUENCY
-                                term_freq_bytes = postings_file.read(4)
-                                term_freq = int.from_bytes(term_freq_bytes, byteorder='big')
+                            index[subliteral].append(current_posting)
+
+                    # # term count stored in first 4 bytes of file
+                    # term_count_bytes = vocab_table_file.read(4)
+                    # term_count = int.from_bytes(term_count_bytes, byteorder='big')
+
+                    # # use linear search for now, will user B+ tree later
+                    # last_position_bytes = vocab_table_file.read(8)
+                    # last_position = int.from_bytes(last_position_bytes, byteorder='big')
                     
-                                current_posting = [last_doc_id, term_freq]
+                    # for i in range(term_count - 1):
+                    #     # READ POSTING POSITION
+                    #     posting_position_bytes = vocab_table_file.read(8)
+                    #     posting_position = int.from_bytes(posting_position_bytes, byteorder='big')
+                    #     # READ VOCAB POSITION
+                    #     vocab_position_bytes = vocab_table_file.read(8)
+                    #     vocab_position = int.from_bytes(vocab_position_bytes, byteorder='big')
+                    #     # READ WORD AT CURRENT VOCAB POSITION
+                    #     term = vocab_file.read(vocab_position-last_position)
+                    #     last_position = vocab_position
+                    #     # READ NUMBER OF DOCS AT CURRENT POSTING POSITION
+                    #     postings_file.seek(posting_position, 0)
+                    #     number_docs_bytes = postings_file.read(4)
+                    #     number_docs = int.from_bytes(number_docs_bytes, byteorder='big')
+                    #     # FOR EACH DOC READ POSTINGS LIST
+                    #     last_doc_id = 0
+
+                    #     # only doc id to index if current vocab term
+                    #     if term.decode() == subliteral:
+                    #         for d in range(number_docs):
+                    #             doc_id_gap_bytes = postings_file.read(4)
+                    #             doc_id_gap = int.from_bytes(doc_id_gap_bytes, byteorder='big')
+                    #             # GET DOCID BY ADDING GAP
+                    #             last_doc_id += doc_id_gap
+                    #             # READ TERM FREQUENCY
+                    #             term_freq_bytes = postings_file.read(4)
+                    #             term_freq = int.from_bytes(term_freq_bytes, byteorder='big')
+                    
+                    #             current_posting = [last_doc_id, term_freq]
                                 
-                                # READ TERM POSITIONS
-                                if positions:
-                                    doc_positions = []
-                                    # if positions desired read them
-                                    for f in range(term_freq):
-                                        position_bytes = postings_file.read(4)
-                                        position = int.from_bytes(position_bytes, byteorder='big')
-                                        doc_positions.append(position)
-                                    current_posting.append(doc_positions)
+                    #             # READ TERM POSITIONS
+                    #             if positions:
+                    #                 doc_positions = []
+                    #                 # if positions desired read them
+                    #                 for f in range(term_freq):
+                    #                     position_bytes = postings_file.read(4)
+                    #                     position = int.from_bytes(position_bytes, byteorder='big')
+                    #                     doc_positions.append(position)
+                    #                 current_posting.append(doc_positions)
 
-                                index[subliteral].append(current_posting)
+                    #             index[subliteral].append(current_posting)
+                    #         break
 
-                    vocab_table_file.seek(0)
-                    vocab_file.seek(0)
-                    postings_file.seek(0)
+                    # vocab_table_file.seek(0)
+                    # vocab_file.seek(0)
+                    # postings_file.seek(0)
         
-        vocab_table_file.close()
-        vocab_file.close()
+        # vocab_table_file.close()
+        # vocab_file.close()
         postings_file.close()
+        conn.close()
         return index
 
     def query_search(self, query, index):
@@ -201,28 +230,21 @@ class DiskIndex(object):
                 combined_postings_lists = sorted(combined_postings_lists, key=lambda t:t[0])
                 docs_with_current_query = []
 
-                # SPLIT POSTINGS BY DOCUMENT ID
                 for key,doc_postings in groupby(combined_postings_lists, itemgetter(0)):
                     doc_postings = list(doc_postings)
                     if len(subliterals) > 1:
-                        # CHECK IF LENGTH OF POSTINGS IS THE SAME AS THE SUBLITERALS
                         if len(doc_postings) == len(subliterals):
                             subliteral_found = True
                             postings = [x[2] for x in doc_postings]
                             for i in range(len(postings)):
                                 postings[i] = [posting - i for posting in postings[i]]
-                                                        
                             results = set.intersection(*map(set, postings))
-                            
                             if len(results) > 0:
                                 docs_with_current_query.append(doc_postings[0][0])
-
                     else:
                         docs_with_current_query.append(doc_postings[0][0])
-
                 docs_with_all_queries.append(docs_with_current_query)
 
-            # INTERSECT DOC IDs WITH SUCCESSFUL QUERIES
             ids_intersect = list(set.intersection(*map(set, docs_with_all_queries)))
             success_doc_ids.extend(ids_intersect)
         
@@ -234,6 +256,7 @@ if __name__ == "__main__":
 
     # index_writer = IndexWriter()
     # index_writer.build_index(indexes[0])
+
     query = '\"a gateway to the wilderness\" + filter'
     disk_index = DiskIndex(path='bin/')
     temp_index = disk_index.retrieve_postings(query)
